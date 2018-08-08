92564482ddaf41fa754469673bae0c69a7ca2f98
hrainen <hrainen@hotmail.com>
2018-08-07 20:08:30 -0700

    Basic UI
    Added player health slider
    Correctly displays red/green colors based on percentage of health
    Added ability for player to take damage and die (currently player dying = debug string)
    Using Roboto font, please don't use default fonts for anything.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+
+    public Slider healthbar;
+    public Text healthValueText;
+
+    // Use this for initialization
+    void Start () {
+        MaxHealth = 100;
+        // set player health to full on game load
+        CurrentHealth = 100;
+
+        healthbar.value = CalcHealth();
+        healthValueText.text = CurrentHealth + "/" + MaxHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            DealDamage(30);
+        }
+		
+	}
+
+    void DealDamage(float dmg)
+    {
+        CurrentHealth -= dmg;
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+
+        // update health after taking dmg
+        healthbar.value = CalcHealth(); 
+        healthValueText.text = CurrentHealth + "/" + MaxHealth;
+
+        
+    }
+
+    float CalcHealth()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+
+    void Die()
+    {
+        CurrentHealth = 0;
+        Debug.Log("Player is dead! X X");
+    }
+}
