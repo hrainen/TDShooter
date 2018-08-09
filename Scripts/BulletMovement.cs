c194537ddc435b06156a9743f6adcf013e24edb4
hrainen <hrainen@hotmail.com>
2018-08-08 23:03:03 -0700

    Simple Menu system
    Added: main menu
    Added: settings menu (currently only has volume slider *not working)
    Added: play game button (works)
    Added: quit button

---
+++ Scripts/BulletMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+    public float speed = 3f;
+    public Quaternion dir;
+    public float damage = 25;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position += new Vector3( Time.deltaTime * 1.0f, 0 ,0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        var enemy = col.gameObject.GetComponent<enemyControl>();
+        if (enemy != null)
+        {
+            enemy.health -= damage;
+        }
+
+        Destroy(this.gameObject);
+        
+    }
+}
---
+++ Scripts/enemyControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyControl : MonoBehaviour {
+    public float health = 100;
+
+    private float rotateSpeed = 1f;
+    private float radius = 3f;
+
+    private Vector3 center;
+    private float angle;
+
+
+	// Use this for initialization
+	void Start () {
+        center = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(health);
+        angle += rotateSpeed * Time.deltaTime;
+        var offset = new Vector3(Mathf.Sin(angle) * radius , Mathf.Cos(angle) * radius, 0);
+        transform.position = center + offset;
+
+        if (health <= 0) // enemy has died
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
